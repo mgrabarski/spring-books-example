e2367df84cc81931504a6f628dc36a4070c0d0c7
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2018-08-26 15:25:58 +0200

    Init commit

---
+++ src/main/java/com/grabarski/mateusz/springfirsttime/SpringFirstTimeApplication.java
@@ -0,0 +1,12 @@
+package com.grabarski.mateusz.springfirsttime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFirstTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFirstTimeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/grabarski/mateusz/springfirsttime/SpringFirstTimeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.grabarski.mateusz.springfirsttime;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringFirstTimeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
