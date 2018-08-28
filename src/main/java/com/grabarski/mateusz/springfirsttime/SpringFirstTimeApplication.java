d9c9eb68526f260028227ae5c093e913be880e19
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2018-08-28 12:24:27 +0200

    simple bean example

--- src/main/java/com/grabarski/mateusz/springfirsttime/SpringFirstTimeApplication.java
+++ src/main/java/com/grabarski/mateusz/springfirsttime/SpringFirstTimeApplication.java
@@ -1,12 +1,17 @@
 package com.grabarski.mateusz.springfirsttime;
 
+import com.grabarski.mateusz.springfirsttime.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringFirstTimeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringFirstTimeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = SpringApplication.run(SpringFirstTimeApplication.class, args);
+
+        MyController myController = applicationContext.getBean(MyController.class);
+        myController.someMessage();
+    }
 }
---
+++ src/main/java/com/grabarski/mateusz/springfirsttime/controllers/MyController.java
@@ -0,0 +1,11 @@
+package com.grabarski.mateusz.springfirsttime.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public void someMessage() {
+        System.out.println("Some message");
+    }
+}
